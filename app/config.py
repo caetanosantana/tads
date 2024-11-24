370483dc68f41f67766fa044a5fcbb91fa850a00
rosaLivia <127208803+rosaLivia@users.noreply.github.com>
2024-11-24 15:08:01 -0400

    configs de banco

--- app/config.py
+++ app/config.py
@@ -7,7 +7,17 @@ class Settings(BaseSettings):
     client_secret: str = "secret"
     algorithm: str = "RS256"
 
+    db_user: str = "postgres"
+    db_password: str = "postgres"
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_name: str = "tadsbanco"
+
     class Config:
         env_file = ".env"
-
+    @property
+    def database_url(self) -> str:
+        """ Retorna a URL de conexão com o banco de dados
+        """
+        return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 settings = Settings()
